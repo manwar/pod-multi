8975e4041f067474de03d56d275da954b365163f
James E Keenan <jkeenan@cpan.org>
2017-03-19 21:58:38 -0400

    Move Pod-Multi into git as pod-multi.
    Add .gitignore, MANIFEST.SKIP, .version_info.pl.

---
+++ .version_info.pl
@@ -0,0 +1,8 @@
+%trans = (
+    version => { old => '0.09', new => '0.10' },
+    date    => { old => 'October 27 2007', new => 'March 19 2017' },
+    copyright => { old => '2007', new => '2017' },
+);
+
+1;
+
