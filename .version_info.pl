8967c2b936e9f321c02eee59628da3a646d3d5a5
James E Keenan <jkeenan@cpan.org>
2017-05-16 12:31:04 -0400

    Again update release dates.

--- .version_info.pl
+++ .version_info.pl
@@ -1,6 +1,6 @@
 %trans = (
     version => { old => '0.09', new => '0.10' },
-    date    => { old => 'October 27 2007', new => 'March 19 2017' },
+    date    => { old => 'March 19 2017', new => 'May 16 2017' },
     copyright => { old => '2007', new => '2017' },
 );
 
